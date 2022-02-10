c0531e845755324ff5efcbd560538fcc5d0cd820
Cdelatorre <carlosdelatorrediaz@gmail.com>
2022-02-10 21:48:27 +0100

    test

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,15 +1,8 @@
 require('dotenv').config()
 const PASSWORD = process.env.PASSWORD;
 
-export default function (req, res) {
+export default async (req, res) => {
   let nodemailer = require('nodemailer')
-
-  const mailData = {
-    "from": "solardelatorre@gmail.com",
-    "to": "solardelatorre@gmail.com",
-    "subject": "SUBSCRIPCIÓN DE CONSUMO SOLAR",
-    "html": req.body.email
-   }
 
   const transporter = nodemailer.createTransport({
     port: "465",
@@ -21,11 +14,37 @@ export default function (req, res) {
     secure: true,
   })
 
-  transporter.sendMail(mailData)
-    .then(() => {
-      console.log('email send!')
-      return res.status(200)
-    })
-    .catch(err => console.log(err))
+  await new Promise((resolve, reject) => {
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+            reject(error);
+        } else {
+            console.log("Server is ready to take our messages");
+            resolve(success);
+        }
+    });
+});
+
+const mailData = {
+  "from": "solardelatorre@gmail.com",
+  "to": "solardelatorre@gmail.com",
+  "subject": "SUBSCRIPCIÓN DE CONSUMO SOLAR",
+  "html": `${req.body.email}`
+ }
+
+
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailData, (err, info) => {
+        if (err) {
+            console.error(err);
+            reject(err);
+        } else {
+            console.log(info);
+            resolve(info);
+        }
+    });
+  });
+  res.status(200).json({ status: "OK" });
   res.end()
 }
