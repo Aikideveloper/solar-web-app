90a3c72fcd6faf99fec8d5b41f7718fc3b8af44c
Aikideveloper <102995188+Aikideveloper@users.noreply.github.com>
2022-04-16 16:37:54 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,9 +74,9 @@ export default function Home() {
       </section>
       <section className="bg-app-primary" id="contact-form">
         <div className="mx-auto justify-content-center container p-5 mb-5">
-          <h3 className="c-dark text-center">
+          <h4 className="c-dark text-center">
             «La radiación solar en España es un privilegio que debemos aprovechar»
-          </h3>
+          </h4>
         </div>
       </section>
 
