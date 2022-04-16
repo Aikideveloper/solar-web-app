cbe292d1c6ee8c73d189e5d14950ac79d904d1e7
Aikideveloper <102995188+Aikideveloper@users.noreply.github.com>
2022-04-16 21:15:18 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -11,6 +11,8 @@ const about = () => {
     <div id="AboutPage" className="pt-5">
       <Head>
         <title>About | Instalación de placas solares | Autoconsumo Solar</title>
+         <meta name="description" content="Solar: Instalar placas solares en tu vivienda y solicita las subvenciones. Ahorro de un 50% en la instalación ¡Infórmate sin compromiso! - Autoconsumo Solar" />
+        <link rel="icon" href="/img/logo_nav.png" />
       </Head>
       <div className="jumbotron jumbotron-fluid mb-0">
         <div className="container p-4">
