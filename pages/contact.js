8dc7c86589a587fbca71c28c21f8377a9e8b91ef
Aikideveloper <102995188+Aikideveloper@users.noreply.github.com>
2022-04-16 15:57:00 +0200

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -7,7 +7,7 @@ const contact = () => {
       <div className="contact-desktop container pt-5 vh-100 d-flex align-items-center">
         <div id="leftHalf" className="d-flex align-items-center justify-content-center ">
           <div className="p-4">
-            <h1 className="pb-3">Contacta con nosotros</h1>
+            <h1 className="pb-3">Solicita tu presupuesto gratuito</h1>
             <Form />
           </div>
         </div>
@@ -17,7 +17,7 @@ const contact = () => {
       <div className="contact-mobile ">
         <div className="contact-overlay container pt-5 d-flex align-items-center">
           <div className="pt-2 pb-4">
-            <h1 className="pb-3 c-white">Contacta con nosotros</h1>
+            <h1 className="pb-3 c-white">Solicita tu presupuesto gratuito</h1>
             <Form />
           </div>
         </div>
