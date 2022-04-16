b4dce3b99f15bcaa9413ab416efd69189f9c4fdc
Aikideveloper <102995188+Aikideveloper@users.noreply.github.com>
2022-04-16 16:34:08 +0200

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -6,13 +6,6 @@ const contact = () => {
   return (
     <div>
       <div className="contact-desktop container pt-5 vh-100 d-flex align-items-center">
-     //jma-ini
-      <Head>
-        <title>Solar | Instalación de placas solares | Autoconsumo Solar</title>
-        <meta name="description" content="Solar: Instalar placas solares en tu vivienda y solicita las subvenciones. Ahorro de un 50% en la instalación ¡Infórmate sin compromiso! - Autoconsumo Solar" />
-        <link rel="icon" href="/img/logo_nav.png" />
-      </Head>
-//jma-fin
         <div id="leftHalf" className="d-flex align-items-center justify-content-center ">
           <div className="p-4">
             <h1 className="pb-3">Solicita tu presupuesto gratuito</h1>
