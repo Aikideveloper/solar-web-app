c08964c08575a0b4cf55c1ba713b0aced9637865
Aikideveloper <aikideveloper@gmail.com>
2022-04-19 19:56:51 +0200

    test

--- pages/_app.js
+++ pages/_app.js
@@ -28,6 +28,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <div id="MyApp">
 
+
+      <Head>
+
       <Script scr={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} strategy="afterInteractive"/>
       <Script id="google-analytics-script" strategy="afterInteractive"
         dangerouslySetInnerHTML={{
@@ -44,7 +47,8 @@ function MyApp({ Component, pageProps }) {
 
       />
 
-      <Head>
+
+
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossOrigin="anonymous"/>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
